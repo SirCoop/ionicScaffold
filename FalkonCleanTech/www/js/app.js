3781171d6c534995224ae487574df379ac83678f
SirCoop <cooper.garym@gmail.com>
2015-10-27 18:01:09 -0400

    Working Ionic Scaffold with abstract state.
    HTML5 Mode off due to 'Failed to load resource' error.
    
    NOTE: <allow-navigation href="http://*/*" /> allows device to access files locally. Otherwise, 'Failed to load resource' errors occur.

--- FalkonCleanTech/www/js/app.js
+++ FalkonCleanTech/www/js/app.js
@@ -2,7 +2,7 @@ angular.module('fcs', ['ionic'])
 
   .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
 
     $stateProvider
       .state('app', {
